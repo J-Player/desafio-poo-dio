244d3abd020c7efaea395a0b6a658c69438cc80b
João Pedro R. Diniz <jp_rd@hotmail.com>
2023-02-14 17:35:58 -0300

    Adicionado método criarDev, uma sobrecarga do método Progredir e uma refatoração no método calcularTotalXp.

--- src/br/com/dio/desafio/dominio/Dev.java
+++ src/br/com/dio/desafio/dominio/Dev.java
@@ -7,6 +7,13 @@ public class Dev {
     private Set<Conteudo> conteudosInscritos = new LinkedHashSet<>();
     private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();
 
+    public static Dev criarDev(String nome, Bootcamp bootcamp) {
+        Dev dev = new Dev();
+        dev.setNome(nome);
+        dev.inscreverBootcamp(bootcamp);
+        return dev;
+    }
+
     public void inscreverBootcamp(Bootcamp bootcamp){
         this.conteudosInscritos.addAll(bootcamp.getConteudos());
         bootcamp.getDevsInscritos().add(this);
@@ -18,23 +25,27 @@ public class Dev {
             this.conteudosConcluidos.add(conteudo.get());
             this.conteudosInscritos.remove(conteudo.get());
         } else {
-            System.err.println("Você não está matriculado em nenhum conteúdo!");
+            System.err.println("Não há matrícula ativa em nenhum conteúdo!");
+        }
+    }
+
+    public void progredir(int quantidade) {
+        if (!this.conteudosInscritos.isEmpty()) {
+            if (quantidade <= this.conteudosInscritos.size()) {
+                for (int i = 0; i < quantidade; i++) progredir();
+            } else {
+                System.err.println("Você não pode progredir mais do que os conteúdos em que está matriculado!");
+            }
+        } else {
+            System.err.println("Não há matrícula ativa em nenhum conteúdo!");
         }
     }
 
     public double calcularTotalXp() {
-        Iterator<Conteudo> iterator = this.conteudosConcluidos.iterator();
-        double soma = 0;
-        while(iterator.hasNext()){
-            double next = iterator.next().calcularXp();
-            soma += next;
-        }
-        return soma;
-
-        /*return this.conteudosConcluidos
+        return this.conteudosConcluidos
                 .stream()
                 .mapToDouble(Conteudo::calcularXp)
-                .sum();*/
+                .sum();
     }
 
 
